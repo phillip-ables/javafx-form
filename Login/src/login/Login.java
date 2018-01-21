2180000232f95b55ac2b353526089a3f1f536b92
Phillip <phillipables1@gmail.com>
2018-01-21 00:29:43 -0500

    create the controls

--- Login/src/login/Login.java
+++ Login/src/login/Login.java
@@ -7,8 +7,14 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -32,6 +38,33 @@ public class Login extends Application {
         //insets top,right,bottom,left(standard)
         grid.setPadding(new Insets(25,25,25,25));
         
+        //creating the controls
+        //uneditable text line assigned to scenetitle
+        Text scenetitle = new Text("Welcome");
+        //inline is appropriate wehre syle is bount to variable but best technique is using a stylesheet
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        //add scenetitle to layout grid
+        //0 column, 0 row, 2 c span, 1 r span
+        grid.add(scenetitle,0,0,2,1);
+        
+        //label object with text users name
+        Label userName = new Label("User Name:");
+        grid.add(userName,0,1);
+        
+        TextField userTextField = new TextField();
+        grid.add(userTextField,1,1);
+        
+        Label pw = new Label("Password:");
+        grid.add(pw,0,2);
+        
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox,1,2);
+        
+        //see the lines for columns rows and gaps if true
+        grid.setGridLinesVisible(true);
+                
+        //set the scene with 300 by 275
+        //default dimensions: minimun size needed
         Scene scene = new Scene(grid,300,275);
         primaryStage.setScene(scene);        
         
