4ff4ca02d65ac75ee1b8015df0244f3782ecf320
Phillip <phillipables1@gmail.com>
2018-01-20 21:26:04 -0500

    GridPane with gap and padding as parent node of scene

--- Login/src/login/Login.java
+++ Login/src/login/Login.java
@@ -8,8 +8,10 @@ package login;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -22,6 +24,15 @@ public class Login extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("JavaFX Welcome");
+        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
+        
+        Scene scene = new Scene(grid,300,275);
+        primaryStage.setScene(scene);        
         
         primaryStage.show();
     }
